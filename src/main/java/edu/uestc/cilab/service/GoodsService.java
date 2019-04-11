d890f2c0377acc13f4e3ad964e09b3bb3ea79b7b
zhangfeng <2498711309@qq.com>
2019-04-11 18:54:23 +0800

    修改接口注释2

--- src/main/java/edu/uestc/cilab/service/GoodsService.java
+++ src/main/java/edu/uestc/cilab/service/GoodsService.java
@@ -13,35 +13,32 @@ public interface GoodsService {
      * 添加商品
      *
      * @param  goods 商品
-     * @author zhangfeng
      * @throws  ExistException
      */
     void add(Goods goods) throws ExistException;
-
     /**
      * 删除商品
      *
      * @param id 商品id
-     * @author zhangfeng
      */
     void delete(Integer id);
 
     /**
      * 更新商品
      * @param goods 商品
-     * @author zhangfeng
      * @throws  ExistException
      */
     void update(Goods goods) throws ExistException ;
 
     /**
      * 查询商品列表
-     * @param goodsType 商品类型
-     * @param title 标题
+     *
      * @param pageNumber 页码
      * @param pageSize 页面大小
      * @param sortName 排序字段
      * @param sortOrder 排序方式
+     * @param goodsType 商品类型
+     * @param title 标题
      * @return PageUtil<Goods>
      */
     PageUtil<Goods> selectByParameter(Integer pageNumber, Integer pageSize, String sortName, String sortOrder, String goodsType, String title);
@@ -49,7 +46,6 @@ public interface GoodsService {
     /**
      * 查询商品详情
      * @param id 商品id
-     * @author zhangfeng
      * @return Goods
      */
     Goods selectById(Integer id);
